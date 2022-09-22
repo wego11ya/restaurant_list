1198692e761df80f10d44562cb95e6a763836201
wego11ya <wego11ya@gmail.com>
2022-09-22 16:18:30 +0800

    Set up express-handlebars

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const exphbs = require("express-handlebars");
 
 app.get("/", (req, res) => {
-  res.send("This is a restaurant list built with Express");
+  res.render("index");
 });
+
+//setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.listen(port, () => {
   console.log(`Express is now listening on localhost:3000`);
