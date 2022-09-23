e4971a45bd7dc580b487503c8c2cd00cdd283c7c
wego11ya <wego11ya@gmail.com>
2022-09-23 13:47:53 +0800

    Set show.handlebars

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.get("/", (req, res) => {
   res.render("index", { restaurants: restaurantList.results });
 });
 
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant });
+});
+
 //setting template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
