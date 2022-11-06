ee36b5a0e0f50e47a232ae36c6dc4b62b7e67bf4
wego11ya <wego11ya@gmail.com>
2022-11-06 16:10:34 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -14,6 +14,10 @@ router.post(
     failureRedirect: "/users/login",
   })
 );
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/users/login");
+});
 router.get("/register", (req, res) => {
   res.render("register");
 });
