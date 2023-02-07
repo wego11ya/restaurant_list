6ea1eabaf44ad447fd239dbc7daeb04ec297dd20
wego11ya <wego11ya@gmail.com>
2023-02-08 00:02:39 +0800

    feat:flash message while login failed

--- config/passport.js
+++ config/passport.js
@@ -10,19 +10,15 @@ module.exports = (app) => {
   app.use(passport.session());
   // 設定本地登入策略
   passport.use(
-    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    new LocalStrategy({ usernameField: "email", passReqToCallback: true}, (req, email, password, done) => {
       User.findOne({ email })
         .then((user) => {
           if (!user) {
-            return done(null, false, {
-              message: "That email is not registered!",
-            });
+            return done(null, false, req.flash('warning_msg', 'The mail is not registered!'));
           }
           bcrypt.compare(password, user.password).then((isMatch) => {
             if (!isMatch) {
-              return done(null, false, {
-                message: "Email or Password incorrect.",
-              });
+              return done(null, false, req.flash('warning_msg', 'Email or password incorrect!'));
             }
             return done(null, user);
           });
